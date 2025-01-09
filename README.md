5bbad4d9702061e90c3161a304347c09209c2b96
Atte Saarinen <atte@blueraven.fi>
2025-01-09 04:08:28 +0200

    Fix / improve cs2 server docker compose and add metamod + matchzy installation

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
-# Servujatkot
+# Laniservut
+
+## Installation
+
+```
+cp .env.example .env
+```
+
+Fill in the values.
+
+Latest metamod download url can be found from https://www.metamodsource.net/downloads.php?branch=dev
+
+Latest cssharp download url can be found from https://github.com/roflmuffin/CounterStrikeSharp/releases (the one the runtime)
+
+Latest matchzy download url can be found from https://github.com/shobhit-pathak/MatchZy/releases/ (the one without cssharp)
+
+```
+source .env
+```
+
+```
+mkdir -p cs2/steam
+```
+
+```
+docker compose up -d
+```
+
+## CS2 installation
+
+Install metamod, cssharp and matchzy
+
+```
+cd cs2
+./setup.sh
+cd ..
+docker compose restart cs2-server
+```
 
 ## CS2 rcon
+
 docker run -it --rm outdead/rcon ./rcon -a cs2.poggers.fi:27015 -p servujatkot "<komento>"
 
 ## Pro Pilkki 2
+
 http://propilkki.net/index.php/pro-pilkki-2/verkkopelien-komennot
--- cs2/setup.sh
+++ cs2/setup.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
 
-GAMEINFO_PATH="./steam/game/csgo/gameinfo.gi"
+GAME_PATH="./steam/game/csgo"
+GAME_INFO="gameinfo.gi"
 
-if [ ! -f "$GAMEINFO_PATH" ]; then
+cd $GAME_PATH
+
+echo "Downloading Metamod..."
+curl -L $METAMOD_RELEASE_URL -o metamod.tar.gz
+echo "Unpacking Metamod..."
+tar -xvf metamod.tar.gz
+echo "Removing Metamod tar file..."
+rm metamod.tar.gz
+echo "Metamod installed successfully."
+
+echo "Downloading CSSharp..."
+curl -L $CSSHARP_RELEASE_URL -o cssharp.zip
+echo "Unpacking CSSharp..."
+unzip cssharp.zip
+echo "Removing CSSharp zip file..."
+rm cssharp.zip
+echo "CSSharp installed successfully."
+
+echo "Downloading MatchZy..."
+curl -L $MATCHZY_RELEASE_URL -o matchzy.zip
+echo "Unpacking MatchZy..."
+unzip matchzy.zip
+echo "Removing MatchZy zip file..."
+rm matchzy.zip
+echo "MatchZy installed successfully."
+
+if [ ! -f "$GAME_INFO" ]; then
   echo "Error: gameinfo.gi file not found."
   exit 1
 fi
 
-if ! grep -q "csgo/addons/metamod" "$GAMEINFO_PATH"; then
-  /usr/bin/sed -i 's/^\(\t\t\tGame\tcsgo\)\([^_]\)/\1\/addons\/metamod\r\n\t\t\tGame\tcsgo\2/' "$GAMEINFO_PATH"
+if ! grep -q "csgo/addons/metamod" "$GAME_INFO"; then
+  /usr/bin/sed -i 's/^\(\t\t\tGame\tcsgo\)\([^_]\)/\1\/addons\/metamod\r\n\t\t\tGame\tcsgo\2/' "$GAME_INFO"
   echo "Metamod plugin activated."
 else
   echo "Metamod plugin is already activated."
